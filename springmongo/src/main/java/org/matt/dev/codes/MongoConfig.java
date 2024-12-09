bbecab0085c3da224e797d9a51d815818c2b2501
Matthew-Gallardo <gallardomatthew8@gmail.com>
2024-12-09 17:14:40 +0800

    Spring + Mongodb integration

--- springmongo/src/main/java/org/matt/dev/codes/MongoConfig.java
+++ springmongo/src/main/java/org/matt/dev/codes/MongoConfig.java
@@ -1,19 +1,23 @@
 package org.matt.dev.codes;
+
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionString;
+
     @Bean
     public MongoClient mongoClient() {
-        String connectionString = "mongodb+srv://gallardomatthew8:rVhbrZXxsy9F0Eav@spring-cluster.eal3k.mongodb.net/?retryWrites=true&w=majority&appName=Spring-Cluster";
         ServerApi serverApi = ServerApi.builder()
                 .version(ServerApiVersion.V1)
                 .build();
